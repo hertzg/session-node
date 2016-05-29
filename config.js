77a06aa439b4e630e1dae2646fd0067d0b157f94
Archil Imnadze <archil@imnadze.ge>
2016-05-29 17:20:13 +0000

    open session

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.port = 7703
+exports.host = '127.0.0.1'
---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http'),
+    url = require('url')
+
+var config = require('./config.js'),
+    Error404Page = require('./lib/Error404Page.js')
+
+var sessions = Object.create(null)
+var users = Object.create(null)
+
+var pages = Object.create(null)
+pages['/'] = require('./lib/IndexPage.js')
+pages['/open'] = require('./lib/OpenPage.js')(sessions, users)
+
+http.createServer((req, res) => {
+    var parsedUrl = url.parse(req.url, true)
+    var page = pages[parsedUrl.pathname]
+    if (page === undefined) page = Error404Page
+    page(req, res, parsedUrl)
+}).listen(config.port, config.host)
